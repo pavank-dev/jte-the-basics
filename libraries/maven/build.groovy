427ee50f7f2d5edca9e77460c251b5881f095428
pavank-dev <64188502+pavank-dev@users.noreply.github.com>
2020-04-27 12:57:31 +0530

    Update build.groovy

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,5 +1,11 @@
-void call(){
-    stage("Maven: Build"){
-        println "build from the maven library"
-    }
+package org.foo
+class Utilities {
+  static def mvn(script, args) {
+    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+  }
+}
+
+@Library('utils') import static org.foo.Utilities.*
+node {
+  mvn this, 'clean package'
 }
