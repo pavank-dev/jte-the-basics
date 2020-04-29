1d5a819b82523318ba5f88d0cfb9367a47927cda
pavank-dev <64188502+pavank-dev@users.noreply.github.com>
2020-04-29 16:50:41 +0530

    Update build.groovy

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -3,7 +3,7 @@ void call(){
         println "build from the gradle library"
        def sout = new StringBuffer(), serr = new StringBuffer()
 
-     def proc ='/root/script.sh'.execute()
+     def proc ='ll /tmp/script.sh'.execute()
 
     proc.consumeProcessOutput(sout, serr)
    proc.waitForOrKill(1000)
